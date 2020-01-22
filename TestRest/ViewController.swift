7d705fafce6d19ff7ae1dd7c9e3b127c1c6dc03e
mobapp11 <mobapp11@student.ehb.be>
2020-01-22 10:23:49 +0100

    add files

---
+++ TestRest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestRest
+//
+//  Created by mobapp11 on 22/01/2020.
+//  Copyright © 2020 mobapp11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
